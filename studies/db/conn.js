0116f7bfa386a7413a0429fdcda1aa05c00787c3
Kiara Victoria <83596877+NegrescoDemon02@users.noreply.github.com>
2024-06-06 17:07:06 -0300

    error 1045
    erro no banco de dados, erro 1045

--- studies/db/conn.js
+++ studies/db/conn.js
@@ -1,6 +1,16 @@
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize('nodemonvc2', 'root', '', {
+const sequelize = new Sequelize('nodemonmvc', 'root', '', {
     host: 'localhost',
     dialect: 'mysql',
 })
+
+try{
+
+    sequelize.authenticate()
+    console.log('Conectamos ao MySQL!')
+}catch(error){
+    console.log(`Não foi possivel se conectar: ${error}`)
+}
+
+exports.default = sequelize
